3581a54f6e14bcf56fecfc8d4781e80dc7da3dc0
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-19 21:50:13 +0300

    * add history service

--- app/history/history.controller.js
+++ app/history/history.controller.js
@@ -5,9 +5,14 @@
   angular.module('gpApp')
     .controller('historyController', historyController);
 
-  function historyController($scope, $state) {
+  function historyController($scope, $state, historyService) {
+    var vm = this;
 
-    
+     historyService.getHistory().then(function(response){
+      vm.historyItems = response.data;
+       console.log('historyItems', vm.historyItems);
+    });
+
   }
 
 })();
