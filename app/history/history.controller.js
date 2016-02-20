e269ecd176e11a07768f6253f3587364502ba35b
Avinar-24 <art.gospodarsky@gmail.com>
2016-02-20 13:34:00 +0300

    Add SortPrice button

--- app/history/history.controller.js
+++ app/history/history.controller.js
@@ -5,8 +5,28 @@
   angular.module('gpApp')
     .controller('historyController', historyController);
 
-  function historyController($scope, $state) {
+  function historyController($scope, $state, historyService) {
     
+    var toggleSorting = 1;
+    var vm = this;
+
+    vm.sortByPrice = sortByPrice;
+
+    historyService.getHistory().then(function(response) {
+      vm.historyItems = response.data;
+      console.log('historyItems', vm.historyItems);
+    });
+
+    function sortByPrice() {
+      toggleSorting *= -1;
+
+      vm.historyItems = _.sortBy(vm.historyItems, function(obj) {
+        return obj.price * toggleSorting;
+      });
+      
+      return vm.historyItems;
+    }
+
   }
 
 })();
---
+++ app/history/history.service.js
@@ -0,0 +1,31 @@
+;
+(function() {
+  angular
+    .module('gpApp')
+    .service('historyService', ['$http', '$q', function($http, $q) {
+      var baseURL = 'http://localhost:3000/history';
+
+      var historyData;
+
+      this.getHistory = function() {
+        if (historyData) {
+          console.log('Get cached history data');
+          return $q.when(historyData);
+        } else {
+          console.log('Send new request for history data');
+          return $http.get(baseURL).then(successCallbackGet, errorCallback);
+        }
+      };
+
+      function successCallbackGet(response) {
+        historyData = response;
+        return historyData;
+      }
+
+      function errorCallback(response) {
+        return "Error: " + response.status + " " + response.statusText;
+      }
+
+
+    }]);
+})();
